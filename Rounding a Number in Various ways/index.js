128a3e548442646e91eb5bdaaf2850756c4d1797
Arunkumar <75107398+Arunkumar-eng@users.noreply.github.com>
2022-10-14 14:06:10 +0530

    Update index.js

--- Rounding a Number in Various ways/index.js
+++ Rounding a Number in Various ways/index.js
@@ -1,6 +1,26 @@
 function roundToInteger(number){
-    var N = parseFloat(number);
-    var y = Math.floor( N + .5 );
+    let N = parseFloat(number);
+    let y = Math.floor( N + .5 );
     document.getElementById("integer").innerHTML = "Rounding a number " + N +  " to integer gives " + y +  " .";
 
+}
+
+function roundToTength(number){
+    let N = parseFloat(number);
+    console.log("Hello World");
+    let y = Math.floor(N * 10 + .5) / 10;
+    document.getElementById("tenth").innerHTML = "Rounding a number " + N + " to tenths position gives " + y + " .";
+
+}
+
+function roundToHundredths(number){
+    let N = parseFloat(number);
+    let y = Math.floor(N * 100 + .5) / 100;
+    document.getElementById("hundred").innerHTML = "Round a number " + N + " to hundredth position gives " + y + " .";
+}
+
+function roundToThousand(number){
+    let N = parseFloat(number);
+    let y = Math.floor(N * 1000 + .5) / 1000;
+    document.getElementById("thousand").innerHTML = "Round a number " + N + " to thousand position gives " + y + " .";
 }
